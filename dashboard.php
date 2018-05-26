c213d7e07745f3d9528ddbbf3c5f3948ecac760f
Jonathan <jonathan16@mhs.if.its.ac.id>
2018-05-26 15:34:11 +0700

    add dashboard

---
+++ dashboard.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.3.1/dist/semantic.min.css">
+
+    <style type="text/css">
+        body {
+            background-color: #DADADA;
+        }
+        body > .grid {
+            height: 100%;
+        }
+        .image {
+            margin-top: -100px;
+        }
+        .column {
+            max-width: 450px;
+        }
+    </style>
+</head>
+<body>
+    <div class="ui middle aligned center aligned grid">
+        <div class="four wide column">
+            <div class="ui vertical fluid attached tabular menu">
+                <a class="item active" data-tab="kitchen">
+                    Kitchen
+                </a>
+                <a class="item" data-tab="storage">
+                    Storage
+                </a>
+                <a class="item" data-tab="food">
+                    Food market
+                </a>
+                <a class="item" data-tab="ingredient">
+                    Ingredients market
+                </a>
+            </div>
+        </div>
+        <div class="twelve wide column">
+            <div class="ui grid">
+                <div class="ui attached tab segment active" data-tab="kitchen">
+                    <div class="ui divided items">
+                        <div class="item">
+                            <div class="image">
+                                <img src="images/wireframe/image.png">
+                            </div>
+                            <div class="content">
+                                <a class="header">Satu</a>
+                                <div class="meta">
+                                    <span>Ini meta / harga</span>
+                                </div>
+                                <div class="description">
+                                    <p>Ini deskripsi</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="image">
+                                <img src="images/wireframe/image.png">
+                            </div>
+                            <div class="content">
+                                <a class="header">Dua</a>
+                                <div class="meta">
+                                    <span>Ini meta / harga</span>
+                                </div>
+                                <div class="description">
+                                    <p>Ini deskripsi</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="image">
+                                <img src="images/wireframe/image.png">
+                            </div>
+                            <div class="content">
+                                <a class="header">Tiga</a>
+                                <div class="meta">
+                                    <span>Ini meta / harga</span>
+                                </div>
+                                <div class="description">
+                                    <p>Ini deskripsi</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="image">
+                                <img src="images/wireframe/image.png">
+                            </div>
+                            <div class="content">
+                                <a class="header">Empat</a>
+                                <div class="meta">
+                                    <span>Ini meta / harga</span>
+                                </div>
+                                <div class="description">
+                                    <p>Ini deskripsi</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="ui attached tab segment" data-tab="storage">
+                    Storage
+                </div>
+                <div class="ui attached tab segment" data-tab="food">
+                    Foods
+                </div>
+                <div class="ui attached tab segment" data-tab="ingredient">
+                    Ingredients
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.3.1/dist/semantic.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $('.menu .item').tab();
+        });
+    </script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,89 +1,89 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Welcome!</title>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.3.1/dist/semantic.min.css">
+    <meta charset="utf-8">
+    <title>Welcome!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.3.1/dist/semantic.min.css">
 
-	<style type="text/css">
-    body {
-      background-color: #DADADA;
-    }
-    body > .grid {
-      height: 100%;
-    }
-    .image {
-      margin-top: -100px;
-    }
-    .column {
-      max-width: 450px;
-    }
-  </style>
+    <style type="text/css">
+        body {
+            background-color: #DADADA;
+        }
+        body > .grid {
+            height: 100%;
+        }
+        .image {
+            margin-top: -100px;
+        }
+        .column {
+            max-width: 450px;
+        }
+    </style>
 </head>
 <body>
-	<div class="ui middle aligned center aligned grid">
-		<div class="column">
-			<h2 class="ui image header">
-				<div class="content">
-					Joel's Kitchen!
-				</div>
-			</h2>
-			<div class="ui top attached two item pointing menu">
+    <div class="ui middle aligned center aligned grid">
+        <div class="column">
+            <h2 class="ui image header">
+                <div class="content">
+                    Joel's Kitchen!
+                </div>
+            </h2>
+            <div class="ui top attached two item pointing menu">
                 <a class="active item" data-tab="login">Login</a>
                 <a class="item" data-tab="signup">Signup</a>
-			</div>
-			<div class="ui bottom attached tab segment active" data-tab="login">
-				<form class="ui form">
-					<div class="ui stacked segment">
-						<div class="field">
-							<div class="ui left icon input">
-								<i class="user icon"></i>
-								<input type="text" name="username" placeholder="Username">
-							</div>
-						</div>
-						<div class="field">
-							<div class="ui left icon input">
-								<i class="lock icon"></i>
-								<input type="password" name="password" placeholder="Password">
-							</div>
-						</div>
-						<div class="ui fluid large submit button">Login</div>
-					</div>
-				</form>
-			</div>
-			<div class="ui bottom attached tab segment" data-tab="signup">
-				<form class="ui form">
-					<div class="ui stacked segment">
-						<div class="field">
-							<div class="ui left icon input">
-								<i class="user icon"></i>
-								<input type="text" name="username" placeholder="Username">
-							</div>
-						</div>
-						<div class="field">
-							<div class="ui left icon input">
-								<i class="envelope icon"></i>
-								<input type="email" name="email" placeholder="E-mail">
-							</div>
-						</div>
-						<div class="field">
-							<div class="ui left icon input">
-								<i class="lock icon"></i>
-								<input type="password" name="password" placeholder="Password">
-							</div>
-						</div>
-						<div class="ui fluid large submit button">Signup</div>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.3.1/dist/semantic.min.js"></script>
-	<script>
-		$(document).ready(function(){
-			$('.menu .item').tab();
-		});
-	</script>
+            </div>
+            <div class="ui bottom attached tab segment active" data-tab="login">
+                <form class="ui form">
+                    <div class="ui stacked segment">
+                        <div class="field">
+                            <div class="ui left icon input">
+                                <i class="user icon"></i>
+                                <input type="text" name="username" placeholder="Username">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <div class="ui left icon input">
+                                <i class="lock icon"></i>
+                                <input type="password" name="password" placeholder="Password">
+                            </div>
+                        </div>
+                        <div class="ui fluid large submit button">Login</div>
+                    </div>
+                </form>
+            </div>
+            <div class="ui bottom attached tab segment" data-tab="signup">
+                <form class="ui form">
+                    <div class="ui stacked segment">
+                        <div class="field">
+                            <div class="ui left icon input">
+                                <i class="user icon"></i>
+                                <input type="text" name="username" placeholder="Username">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <div class="ui left icon input">
+                                <i class="envelope icon"></i>
+                                <input type="email" name="email" placeholder="E-mail">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <div class="ui left icon input">
+                                <i class="lock icon"></i>
+                                <input type="password" name="password" placeholder="Password">
+                            </div>
+                        </div>
+                        <div class="ui fluid large submit button">Signup</div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.3.1/dist/semantic.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $('.menu .item').tab();
+        });
+    </script>
 </body>
 </html>
