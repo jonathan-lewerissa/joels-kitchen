f021b9af492e0c0f44da5ccd7422d17ef84255cf
nurlitadf <nurlitadf17@gmail.com>
2018-05-27 23:27:10 +0700

    update buy bahan

--- buybahan.php
+++ buybahan.php
@@ -1,8 +1,8 @@
 <?php
 	session_start();
 	$conn = mysqli_connect('localhost', 'root', '', "vkvxweok_mbd_05111640000092") or die($error);
-	$sql = "call sp_jual_bahan(".$_GET["id_bahan"].", ".$_SESSION["idplayer"].")";
+	$sql = "call sp_beli_bahan(".$_POST["id_bahan"].", ".$_POST["id_penjual"].", ".$_SESSION["idplayer"].")";
 	echo $sql;
 	$N=mysqli_query($conn,$sql);
-	header("Location:dashboard.php");
+	//header("Location:dashboard.php");
 ?>
--- ingredientmarket.php
+++ ingredientmarket.php
@@ -5,6 +5,12 @@
     $Q = mysqli_query($conn,$sql);
     while($val = mysqli_fetch_assoc($Q)){
         $conn = mysqli_connect('localhost', 'root', '', "vkvxweok_mbd_05111640000092") or die($error);
+        $sq ="call sp_show_detil_player(".$val["idplayer"].")";
+        //echo $sq;
+        $qu = mysqli_query($conn,$sq);
+        $va = mysqli_fetch_assoc($qu);
+        if($va["username"]==$_SESSION["username"]) continue;
+        $conn = mysqli_connect('localhost', 'root', '', "vkvxweok_mbd_05111640000092") or die($error);
         $sql ="call sp_show_detil_bahan(".$val["idbahan"].")";
         $q = mysqli_query($conn,$sql);
         $v = mysqli_fetch_assoc($q);
@@ -27,8 +33,10 @@
         echo            "<p>Seller : ".$va["username"]."</p>";
         echo            "</div>";
         echo            "<div class='ui centered grid' style='display: flex; margin-top: 35px'>";
-        echo                "<form action='buybahan.php'>";
-        echo                    "<button class='ui positive button'>Buy</button>";
+        echo                "<form action='buybahan.php' method='post'>";
+        echo "<input type='hidden' name='id_bahan' value=".$val["idbahan"].">";
+        echo "<input type='hidden' name='id_penjual' value=".$val["idplayer"].">";
+        echo                    "<button type='submit' class='ui positive button'>Buy</button>";
         echo                "</form>";
         echo            "</div>";
         echo        "</div>";
